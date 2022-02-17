30dadbe6d4ee29fbdb412a632d798e464e834943
hao.wei <weihao>
2022-02-17 15:52:49 +0800

    修改index查询

--- migrator.go
+++ migrator.go
@@ -307,8 +307,8 @@ func (m Migrator) HasIndex(value interface{}, name string) bool {
 
 		return m.DB.Raw(
 			"SELECT COUNT(*) FROM USER_INDEXES WHERE TABLE_NAME = ? AND INDEX_NAME = ?",
-			stmt.Table,
-			name,
+			m.Migrator.DB.NamingStrategy.TableName(stmt.Table),
+			m.Migrator.DB.NamingStrategy.IndexName("", name),
 		).Row().Scan(&count)
 	})
 
